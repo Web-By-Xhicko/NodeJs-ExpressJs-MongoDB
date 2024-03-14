8669db7dd7406023ff38820e6bb4be414a8a6915
Xhicko <otagadaisaac@gmail.com>
2024-03-14 07:59:24 +0100

    Created The POST Request Api

--- First_Project/methods/delete-request.js
+++ First_Project/methods/delete-request.js
@@ -1,3 +1,5 @@
 module.exports = (req,res) => {
-
+   let baseUrl = req.url.substring(0, req.url.lastIndexOf("/")+1)
+   let id = req.url.split("/")[3]
+   const regexV4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i)
 }
--- First_Project/methods/post-request.js
+++ First_Project/methods/post-request.js
@@ -1,3 +1,29 @@
-module.exports = (req,res) => {
+const crypto = require("crypto")
+const requestBodyParser = require("../util/body-parser")
+const writeToFile  =  require("../util/write-to-file")
 
+module.exports = async (req,res) => {
+   if(req.url === "/api/movies"){
+      try{
+            let body = await requestBodyParser(req)
+            body.id = crypto.randomUUID()
+            req.movies.push(body)
+            writeToFile(req.movies)
+            res.writeHead(201, {"Content-Type": "application/json"})
+            res.end() 
+         }
+      catch(err){
+         console.log(err);
+         res.writeHead(400, {"Content-Type": "application/json"})
+         res.end(JSON.stringify({
+            title: "Validation Failed",
+            message:"Request body is  Not Valid"}))
+      }
+   }
+   else{
+      res.writeHead(404, {"Content-Type" : "application/json"})
+      res.end(JSON.stringify({
+         title: "Bad Request",
+         message:"Route Not Found"}))
+   }
 }
