ac869410e62987aa36ac9e633b634c3f6fe2a9f2
Xhicko <otagadaisaac@gmail.com>
2024-03-14 07:16:51 +0100

    initial Commit

---
+++ First_Project/server.js
@@ -0,0 +1,38 @@
+const http = require("http")
+
+const getReq = require("./methods/get-request")
+const postReq = require("./methods/post-request")
+const putReq = require("./methods/put-request")
+const deleteReq = require("./methods/delete-request")
+let movies = require("./data/movies.json")
+
+const PORT = process.env.PORT || 5001
+
+const server = http.createServer((req,res) => {
+req.movies = movies
+   switch(req.method){
+      case 'GET':
+         getReq(req, res)
+         break
+      case 'POST':
+         postReq(req, res)
+         break
+      case 'PUT':
+         putReq(req, res)
+         break
+      case 'DELETE':
+         deleteReq(req, res)
+         break
+      default:
+         res.statusCode = 404
+         res.setHeader("content-type", "application/json")
+         res.write(JSON.stringify({
+            title: "Bad Request",
+            message:"Route Not Found"}))
+         res.end()
+   }
+})
+
+server.listen(PORT, () => {
+   console.log(`Server stared on ${PORT}`)
+})
