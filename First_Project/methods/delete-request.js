4f6c62769e5478b4860b91e54ebd33a28d77e0de
Xhicko <otagadaisaac@gmail.com>
2024-03-14 08:39:05 +0100

    Created PUT API Request and Ended Vanilla Nodejs Project

--- First_Project/methods/delete-request.js
+++ First_Project/methods/delete-request.js
@@ -20,6 +20,7 @@ module.exports = (req,res) => {
          res.write(JSON.stringify({
             title: "Not Found",
             message:"Movie Not Found"}))
+         res.end()
       }
       else{
          req.movies.splice(index, 1)
@@ -27,5 +28,11 @@ module.exports = (req,res) => {
          res.writeHead(204, {"Content-Type": "application/json"})
          res.end(JSON.stringify(req.movies))
       }
+   }
+   else{
+      res.writeHead(404, {"Content-Type" : "application/json"})
+      res.end(JSON.stringify({
+         title: "Bad Request",
+         message:"Route Not Found"}))
    }
 }
