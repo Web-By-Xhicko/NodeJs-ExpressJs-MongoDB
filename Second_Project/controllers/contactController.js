87ccda837aaa5664e6b3ac3485c34d046b130406
Xhicko <otagadaisaac@gmail.com>
2024-03-19 17:18:18 +0100

    Successfully connected to mongoose database

--- Second_Project/controllers/contactController.js
+++ Second_Project/controllers/contactController.js
@@ -1,11 +1,69 @@
+const asyncHandler = require("express-async-handler")
+
 //@desc Get all Contacts 
 //@route GET /api/contacts
 //@access public
-const getContact = (req, res) => {
-   res.status(200).json({
-      message: "Get All Contacts"
-   })
-}
+const getContacts = asyncHandler(
+   async (req, res) => {
+      res.status(200).json({
+         message: "Get All Contacts"
+      })
+   }
+) 
+
+//@desc Create a new Contacts 
+//@route POST /api/contacts
+//@access public
+const createContact = asyncHandler(
+   async (req, res) => {
+      console.log("The Request Body is :", req.body);
+      const {name, email, phone} = req.body
+      if(!name || !email || !phone){
+         res.status(400)
+         throw new Error("All Fields Are Required!")
+      }
+      res.status(201).json({
+         message: "Create A Contact"
+      })
+   }
+)
+
+//@desc Get Individual Contact 
+//@route GET /api/contacts/:id
+//@access public
+const getContact = asyncHandler(
+   async (req, res) => {
+      res.status(200).json({
+         message: `Get Contact For ${req.params.id}`
+      })
+   }
+)
+
+//@desc Update Individual Contact 
+//@route PUT /api/contacts/:id
+//@access public
+const updateContact = asyncHandler(
+   async (req, res) => {
+      res.status(200).json({
+         message:  `Update Contact For ${req.params.id}`
+      })
+   }
+   
+)
+//@desc Delete Individual Contact 
+//@route DELETE /api/contacts/:id
+//@access public
+const deleteContact = asyncHandler(
+   async (req, res) => {
+      res.status(200).json({
+         message: `Delete  Contact For ${req.params.id}`
+      })
+   }
+)
 
 
-module.exports = {getContact}
+module.exports = {getContacts,
+                  createContact,
+                  getContact,
+                  updateContact,
+                  deleteContact }
--- Second_Project/routes/contactsRoutes.js
+++ Second_Project/routes/contactsRoutes.js
@@ -1,31 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
+const {getContacts, 
+       createContact, 
+       getContact, 
+       updateContact, 
+       deleteContact} = require("../controllers/contactController")
 
-router.route("/").get()
 
-router.route("/").post((req, res) => {
-   res.status(200).json({
-      message: "Create A Contact"
-   })
-})
+router.route("/").get(getContacts).post(createContact)
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 
-router.route("/:id").get((req, res) => {
-   res.status(200).json({
-      message: `Get Contact For ${req.params.id}`
-   })
-})
 
-router.route("/:id").put((req, res) => {
-   res.status(200).json({
-      message:  `Update Contact For ${req.params.id}`
-   })
-})
 
-router.route("/:id").delete((req, res) => {
-   res.status(200).json({
-      message: `Delete  Contact For ${req.params.id}`
-   })
-})
 
 module.exports = router 
