3878aed36a64418e2c08608cf8b00ddea6978148
Xhicko <otagadaisaac@gmail.com>
2024-03-21 06:31:47 +0100

    Finished Creating User Registration Endpoint

--- Second_Project/controllers/userController.js
+++ Second_Project/controllers/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler")
+const bcrypt = require("bcrypt")
 const User = require("../models/userModel")
 
 //@desc Register Users
@@ -9,9 +10,31 @@ const registerUser = asyncHandler(
       const {username, email, password} = req.body
       if(!username || !email || !password){
          res.status(400)
-         throw new Error("all fields Are Required")
+         throw new Error("All fields Are Required")
+      }
+      const userAvailable = await User.findOne({email})
+      if(userAvailable){
+         res.status(400)
+         throw new Error("User Already Registered")
       }
 
+      const hashedPassword = await bcrypt.hash(password, 10)
+      console.log("Hashed Password:", hashedPassword);
+
+      const user = await User.create({
+         username,
+         email,
+         password: hashedPassword,
+      })
+
+      console.log(`User Created: ${user}`);
+      if(user){
+         res.status(201).json({_id: user.id, email: user.email})
+      }
+      else{
+         res.status(400)
+         throw new Error("User Data Is Not Valid")
+      }
       res.json({message: "Register The User"})}
 )
 
